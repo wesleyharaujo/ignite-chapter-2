7496f5caed43605b0517f5abbb37605ebf284fbc
Wesley <wesleyharaujo@hotmail.com>
2022-05-23 00:29:42 -0300

    first commit

---
+++ src/services/CreateSpecificationService.ts
@@ -0,0 +1,10 @@
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private categoriesRepository)
+}
+
+export { CreateSpecificationService };
